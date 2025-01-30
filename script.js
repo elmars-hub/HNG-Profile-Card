dd0831f88c368847372775a5c2557f5f43cb708e
Martin Ifeanyi <martinsifeanyi247@gmail.com>
2025-01-30 04:10:05 +0100

    time

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function updateTime() {
   const utcTimeElement = document.getElementById("utc-time");
   setInterval(() => {
     const now = new Date();
-    utcTimeElement.textContent = `Current UTC Time: ${now.toUTCString()}`;
+    utcTimeElement.textContent = `${now.toUTCString()}`;
   }, 1000);
 }
 updateTime();
